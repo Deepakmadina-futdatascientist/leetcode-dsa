1c1b798b22ef9cff7556885f5b0beed9c0c68173
Deepakmadina-futdatascientist <madinadeepak5@gmail.com>
2026-02-04 19:57:18 +0530

    Add brute force solution for Two Sum

---
+++ Arrays/01_two_sum_bruteforce.py
@@ -0,0 +1,11 @@
+# LeetCode 1: Two Sum
+# Approach: Brute Force
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
